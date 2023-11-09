40b769d1dedb6bdfbfbae219254ca2dd14688086
GUSTAVO AQUINO DE OLIVEIRA <gustavo.aoliveira34@senacsp.edu.br>
2023-11-09 10:57:09 -0300

    Estrutura inicial adicionada

---
+++ dropping/src/main/java/br/com/dropping/DroppingApplication.java
@@ -0,0 +1,13 @@
+package br.com.dropping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroppingApplication.class, args);
+	}
+
+}
---
+++ dropping/src/test/java/br/com/dropping/DroppingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dropping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
